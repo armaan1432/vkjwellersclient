ae1543de1c4d1ef7a7aa64b43cb148c3a2c41506
Armaan Shukla <142747999+armaan1432@users.noreply.github.com>
2025-11-21 00:06:59 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ export const authDataContext = createContext();
 
 function AuthContext({ children }) {
   // Use the correct environment variable for production
-  const serverUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
+  const serverUrl = import.meta.env.VITE_API_URL || "http://localhost:10000";
 
   return (
     <authDataContext.Provider value={{ serverUrl }}>
